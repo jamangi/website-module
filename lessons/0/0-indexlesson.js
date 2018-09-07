3cb53e82a0a2e6a9183d525e1f9a4acd84b7429f
rubyshadows <343@holbertonschool.com>
2018-09-06 20:14:45 -0700

    get img frame working

--- lessons/0/0-indexlesson.js
+++ lessons/0/0-indexlesson.js
@@ -1,1 +1,20 @@
 let lesson_num = 1;
+let frame = document.getElementById('img-frame');
+
+let instructions = document.getElementsByClassName('instruction-text');
+for (let instruct of instructions) {
+    instruct.addEventListener('mouseenter', populateFrame);
+    instruct.addEventListener('mouseleave', emptyFrame);
+}
+
+function populateFrame() {
+    let img = document.createElement('img');
+    img.setAttribute('src', this.getAttribute('data-img'));
+    img.setAttribute('id', 'img-frame-img');
+    frame.append(img);
+}
+
+function emptyFrame() {
+    let img = document.getElementById('img-frame-img');
+    frame.removeChild(img);
+}
